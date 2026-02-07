1c911f995169da38095f3b861d5b820515a3e00c
K-zoe <kaki.4261.zoes@gmail.com>
2026-02-07 22:21:59 +0900

    【修正】mypageの修正。作成したコース一覧追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,8 @@ from .views import (
     SignupPageView,
     LoginPageView,
     LogoutPageView,
-    MyPageView,
+    CurrentCoursesView,
+    MyCreatedCoursesView,
     ProfileEditView
 )
 
@@ -11,6 +12,8 @@ urlpatterns = [
     path('signup/', SignupPageView.as_view(), name = 'signup'),
     path('login/', LoginPageView.as_view(), name = 'login'),
     path('logout/', LogoutPageView.as_view(), name = 'logout'),
-    path('mypage/', MyPageView.as_view(), name = 'mypage'),
+    path('mypage/', CurrentCoursesView.as_view(), name = 'mypage_current_courses'),#mypageの初期表示に学習中のコースを表示させるようにしている。
+    path('my_created_courses/', MyCreatedCoursesView.as_view(), name = 'my_created_courses'),
+
     path('profile_edit/', ProfileEditView.as_view(), name = 'profile_edit'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,8 @@ from django.contrib.auth.views import LoginView,LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import SignUpForm,LoginForm,UserForm,ProfileForm
 from django.contrib.auth import login,authenticate
-from .models import Profile,User
+from accounts.models import Profile,User
+from quizzes.models import Course
 from django.db import transaction
 from django.views.csrf import csrf_failure
 
@@ -18,7 +19,7 @@ def csrf_failure(request, reason = ""):
 class SignupPageView(CreateView):
     form_class = SignUpForm
     template_name = 'accounts/signup.html'
-    success_url = reverse_lazy('mypage')
+    success_url = reverse_lazy('mypage_current_courses')
 
     def form_valid(self, form):
         with transaction.atomic():
@@ -34,14 +35,15 @@ class SignupPageView(CreateView):
 class LoginPageView(LoginView):
     template_name = 'accounts/login.html'
     authentication_form = LoginForm
-    next_page = reverse_lazy('mypage')
+    next_page = reverse_lazy('mypage_current_courses')
 
 class LogoutPageView(LogoutView):
     next_page = reverse_lazy('login')
 
-class MyPageView(LoginRequiredMixin, ListView):
+class CurrentCoursesView(LoginRequiredMixin, ListView):
+    #マイページを開いた最初の表示
     #model = ''#TODO: 解いている問題を表示するようにする。
-    template_name = 'accounts/mypage.html'
+    template_name = 'accounts/current_courses.html'
     paginate_by = None #TODO: ページネーション機能を追加
 
     def get_context_data(self, **kwargs):
@@ -52,6 +54,21 @@ class MyPageView(LoginRequiredMixin, ListView):
         #TODO: 解いている問題のリスト作成が完了したら、修正する。
         return None#super().get_queryset()
     
+class MyCreatedCoursesView(LoginRequiredMixin, ListView):
+    model = Course
+    context_object_name = 'courses'
+    template_name = 'accounts/my_created_courses.html'
+    paginate_by = 2
+    ordering = ['-pk']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user = self.request.user)
+
 class ProfileEditView(LoginRequiredMixin, View):
     template_name = 'accounts/profile_edit.html'
 
@@ -71,7 +88,7 @@ class ProfileEditView(LoginRequiredMixin, View):
                 user_form.save()
                 profile_form.save()
 
-                return redirect('mypage')
+                return redirect('mypage_current_courses')
         else:
             context = {'user_form':user_form, 'profile_form':profile_form}
             return render(request, self.template_name, context)
