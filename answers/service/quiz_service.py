a13d7033d56fe4eb151a0ef61bc74016efc8b55d
K-zoe <kaki.4261.zoes@gmail.com>
2026-04-05 23:20:57 +0900

    【修正】mixinではなくserviceとして作成しなおし

--- answers/service/quiz_service.py
+++ answers/service/quiz_service.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import get_object_or_404
 from quizzes.models import (
     Course,
     Quiz,
@@ -13,15 +13,18 @@ from answers.models import(
 import random
 from django.db import IntegrityError
 
-class QuizSessionMixin:
-    def get_session(self,quiz) -> QuizSession:
+class QuizService:
+    #NOTE: クイズセッションの管理を行う。
+    @staticmethod
+    def get_session(user,quiz) -> QuizSession:
         session = QuizSession.objects.filter(
-            user = self.request.user,
+            user = user,
             quiz = quiz,
         ).first()
         return session
     
-    def create_session(self, quiz) -> QuizSession:
+    @staticmethod
+    def create_session(user,quiz) -> QuizSession:
         #TODO:quizの登録だけでquiestionがない可能性があるので対応が必要
         question_list = list(
             Question.objects.filter(
@@ -35,31 +38,34 @@ class QuizSessionMixin:
             raise ValueError('このクイズには問題がありません。')
         
         session = QuizSession.objects.create(
-            user = self.request.user,
+            user = user,
             quiz = quiz,
             question_order = question_list
         )
         return session
     
-    def delete_session(self, session_pk):
+    @staticmethod
+    def delete_session(user, session_pk):
         QuizSession.objects.filter(
             pk = session_pk,
-            user = self.request.user
+            user = user
         ).delete()
 
-    def check_and_get_session(self, quiz):
-        session = self.get_session(quiz)
+    @staticmethod
+    def check_and_get_session(user, quiz):
+        session = QuizService.get_session(user, quiz)
         if session:
             if session.is_active is False or session.finished_at:
                 #再挑戦。
-                self.delete_session(session.pk)
-                session = self.create_session(quiz)
+                QuizService.delete_session(user, session.pk)
+                session = QuizService.create_session(user, quiz)
         else:
-            session = self.create_session(quiz)
+            session = QuizService.create_session(user, quiz)
 
         return session
     
-    def next_or_finish_question(self, session):
+    @staticmethod
+    def next_or_finish_question(session):
         current_index = session.current_index + 1
         
         total = len(session.question_order)
@@ -72,24 +78,48 @@ class QuizSessionMixin:
         else:
             session.current_index+=1
             session.save(update_fields = ['current_index'])
+
+    @staticmethod
+    def get_prev_index(url_index):
+        if url_index == 0:
+            return None
+        
+        elif url_index > 0:
+            return url_index -1
+
+    @staticmethod
+    def get_next_index(session, url_index, current_index):
+
+        if url_index == current_index and session.finished_at:
+            return None
+        
+        elif url_index + 1 == current_index and session.finished_at is None:
+            return None
+
+        elif url_index < current_index and url_index >= 0:
+            return url_index + 1
+        
+        else:
+            return None
     
-class QuizObjectMixin:
-    def get_quiz(self, kwargs) -> Quiz:
+    @staticmethod
+    def get_quiz(course_uuid, quiz_uuid) -> Quiz:
         #NOTE: 公開フラグが有効になっているものだけを返す。
         course = get_object_or_404(
             Course,
-            uuid = kwargs.get('course_uuid'),
+            uuid = course_uuid,
             is_public = True
         )
         quiz = get_object_or_404(
             Quiz,
             course = course,
-            uuid = kwargs.get('quiz_uuid'),
+            uuid = quiz_uuid,
             is_public = True
         )
         return quiz
     
-    def get_question(self, session) -> Question | None:
+    @staticmethod
+    def get_question(session) -> Question | None:
         #NOTE:問題表示の取得時だけ使用する。回答時の取得は別に用意する。
         for idx in range(session.current_index, len(session.question_order)):
             question = Question.objects.filter(
@@ -105,10 +135,8 @@ class QuizObjectMixin:
 
         return None
 
-class AnswerCheckMixin:
-    #NOTE:　回答チェック
-    
-    def check_answer(self, session, user_choice):
+    @staticmethod
+    def check_answer(session, user_choice):
         question_pk = session.question_order[session.current_index]
         question = Question.objects.filter(pk = question_pk).first()
         choices = Choice.objects.filter(
--- answers/views/answer.py
+++ answers/views/answer.py
@@ -2,47 +2,30 @@ from django.shortcuts import render,get_object_or_404, redirect
 from django.http import Http404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
-from answers.mixins.attempt import (
-    QuizSessionMixin,
-    QuizObjectMixin,
-    AnswerCheckMixin
-)
-from quizzes.models import (
-    Course,
-    Quiz,
-    Question,
-    Choice
-)
-from answers.models import(
-    QuizSession,
-    Answer
-)
-from django.db import transaction
+from answers.service.quiz_service import QuizService
+from quizzes.models import Question, Choice
+from answers.models import Answer
 
-class AnswerAttempView(
-    LoginRequiredMixin,
-    QuizSessionMixin,
-    QuizObjectMixin,
-    AnswerCheckMixin,
-    View
-):
+class AnswerAttempView(LoginRequiredMixin, QuizService, View):
     template_name = 'answers/answer.html'
 
     def dispatch(self, request, *args, **kwargs):
-        self.quiz = self.get_quiz(kwargs)
+        course_uuid = kwargs.get('course_uuid')
+        quiz_uuid = kwargs.get('quiz_uuid')
+        self.quiz = self.get_quiz(course_uuid, quiz_uuid)
+        self.user = request.user
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        session = self.check_and_get_session(self.quiz)
+        session = self.check_and_get_session(self.user, self.quiz)
         question = self.get_question(session)
         if question is None:
-            #NOTE: 
-            return render()
+            raise Http404()
         context = {'question': question}
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        session = self.get_session(self.quiz)
+        session = self.get_session(self.user, self.quiz)
         user_choice = request.POST.getlist('choice')
         #NOTE: session情報を前提にする。
         self.check_answer(session, user_choice)
@@ -56,36 +39,18 @@ class AnswerAttempView(
         #status = self.next_or_finish_question(session)
         return redirect('answer_feedback', kwargs.get('course_uuid'), kwargs.get('quiz_uuid'), url_index)
 
-class AnswerFeedbackView(LoginRequiredMixin, QuizSessionMixin, QuizObjectMixin, View):
+class AnswerFeedbackView(LoginRequiredMixin, QuizService, View):
     template_name = 'answers/answer_feedback.html'
 
     def dispatch(self,request, *args, **kwargs):
-        self.quiz = self.get_quiz(kwargs)
+        course_uuid = kwargs.get('course_uuid')
+        quiz_uuid = kwargs.get('quiz_uuid')
+        self.quiz = self.get_quiz(course_uuid, quiz_uuid)
+        self.user = request.user
         return super().dispatch(request, *args, **kwargs)    
 
-    def get_prev_index(self, session, url_index, current_index):
-        if url_index == 0:
-            return None
-        
-        elif url_index > 0:
-            return url_index -1
-        
-    def get_next_index(self, session, url_index, current_index):
-
-        if url_index == current_index and session.finished_at:
-            return None
-        
-        elif url_index + 1 == current_index and session.finished_at is None:
-            return None
-
-        elif url_index < current_index and url_index >= 0:
-            return url_index + 1
-        
-        else:
-            return None
-
     def get(self, request, *args, **kwargs):
-        session = self.get_session(self.quiz)
+        session = self.get_session(self.user, self.quiz)
         current_index = session.current_index
         url_index = kwargs.get('index')
 
@@ -95,7 +60,7 @@ class AnswerFeedbackView(LoginRequiredMixin, QuizSessionMixin, QuizObjectMixin, 
             raise Http404()
         
         #画面に表示させるURL制御
-        prev_index = self.get_prev_index(session, url_index, current_index)
+        prev_index = self.get_prev_index(url_index)
         next_index = self.get_next_index(session, url_index, current_index)
 
         question_pk = session.question_order[url_index]
@@ -104,7 +69,6 @@ class AnswerFeedbackView(LoginRequiredMixin, QuizSessionMixin, QuizObjectMixin, 
             pk = question_pk
         )
         
-
         choice = Choice.objects.filter(
             question = question
         ).all()
@@ -126,4 +90,5 @@ class AnswerFeedbackView(LoginRequiredMixin, QuizSessionMixin, QuizObjectMixin, 
             'prev_index':prev_index,
             'next_index':next_index
         }
+        
         return render(request, self.template_name, context)
