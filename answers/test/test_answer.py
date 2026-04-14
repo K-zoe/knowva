13970ccb3d2013bf59d9a278d86c039a9914f7f1
K-zoe <kaki.4261.zoes@gmail.com>
2026-04-14 23:54:53 +0900

    【作成】answerのテストコード作成

--- answers/test/test_answer.py
+++ answers/test/test_answer.py
@@ -1,14 +1,53 @@
-from django.test import TestCase
+from test.base import BaseTest
 from accounts.models import User
+from django.urls import reverse
 
-class BaseTest(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username = 'login_user',
-            email = 'login_user@co.jp',
-            password = 'test12345'
+class AnswerTest(BaseTest):
+    def create_course(self, question_count = 3, is_public = True):
+        #NOTE:回答用のコース作成。
+        course = self.course_create()
+        quiz = self.quiz_create(course)
+        
+        for i in range(question_count):
+            question = self.question_create(quiz)
+            self.choice1_create(question)
+            self.choice2_create(question)
+
+        #回答で表示でのは公開になっているコースと問題集だけなので公開にする。
+        course.is_public = is_public
+        course.save()
+        quiz.is_public = is_public
+        quiz.save()
+
+        return course, quiz
+    
+    def test_answer_attempt_view_is_public_success(self):
+        #NOTE:コースと問題集が公開されている問題だけが表示されるかテスト。
+        course, quiz = self.create_course()
+        response = self.client.get(
+            reverse(
+                'answer_attempt',
+                kwargs = {
+                    'course_uuid': course.uuid,
+                    'quiz_uuid': quiz.uuid
+                }
+            )
         )
-        self.client.login(
-            username = 'login_user@co.jp',
-            password = 'test12345'
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_answer_attempt_view_is_not_public_fail(self):
+        #NOTE:コースと問題集が公開されていない問題は表示されないかテスト。
+        course, quiz = self.create_course(is_public = False)
+        response = self.client.get(
+            reverse(
+                'answer_attempt',
+                kwargs = {
+                    'course_uuid': course.uuid,
+                    'quiz_uuid': quiz.uuid
+                }
+            )
         )
+
+        self.assertEqual(response.status_code, 404)
+
