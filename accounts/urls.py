00c501ff9b70b3148a7624dd3947bba0a337835f
K-zoe <kaki.4261.zoes@gmail.com>
2026-05-07 23:25:36 +0900

    【修正】回答画面などの下に各画面に戻るリンク追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('signup/', SignupPageView.as_view(), name = 'signup'),
     path('login/', LoginPageView.as_view(), name = 'login'),
     path('logout/', LogoutPageView.as_view(), name = 'logout'),
-    path('mypage/', CurrentCoursesView.as_view(), name = 'mypage_current_courses'),#mypageの初期表示に学習中のコースを表示させるようにしている。
+    path('mypage/', CurrentCoursesView.as_view(), name = 'mypage_current_courses'),
     path('my_created_courses/', MyCreatedCoursesView.as_view(), name = 'my_created_courses'),
 
     path('profile_edit/', ProfileEditView.as_view(), name = 'profile_edit'),
